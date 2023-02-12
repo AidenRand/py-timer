1a15e8191aebc543ab36e3018f5b4047feaad891
Cortrax <aidenrphotos@gmail.com>
2023-02-12 17:16:18 -0500

    Add styling

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import time
 from tkinter import *
 from tkinter import messagebox
+from tkinter import font
 
 root = Tk()
 
-root.geometry('200x200')
-root.title('Pomodoro Timer')
+root.geometry('200x175')
+root.title('')
+root.configure(background='#2B2B2B')
 
 hour = StringVar()
 minute = StringVar()
@@ -19,13 +21,13 @@ second.set('00')
 # Get user input
 hourEntry = Entry(root, width=3, font=('Arial', 18, ''),
                   textvariable=hour)
-hourEntry.place(x=80, y=20)
+hourEntry.place(x=30, y=135)
 minuteEntry = Entry(root, width=3, font=('Arial', 18, ''),
                   textvariable=minute)
-minuteEntry.place(x=130, y=20)
+minuteEntry.place(x=80, y=135)
 secondEntry = Entry(root, width=3, font=('Arial', 18, ''),
                   textvariable=second)
-secondEntry.place(x=180, y=20)
+secondEntry.place(x=130, y=135)
 
 def submit():
     try:
@@ -42,3 +44,18 @@ def submit():
         hour.set('{0:2d}'.format(hours))
         minute.set('{0:2d}'.format(mins))
         second.set('{0:2d}'.format(secs))
+        
+        root.update()
+        time.sleep(1)
+
+        if (temp == 0):
+            messagebox.showinfo('Time Countdown', 'Session Finished')
+        
+        temp -= 1
+    
+btnFont = font.Font(family='Helvetica', size=15)
+btn = Button(root, text='START', bd='5', fg='#575757', height=4, width=12, font=btnFont,
+             command = submit)
+btn.place(x = 27, y = 10)
+
+root.mainloop()
