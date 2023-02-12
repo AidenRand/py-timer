d183f9629e3a3aa3134ac6d5d42566ccded63831
Cortrax <aidenrphotos@gmail.com>
2023-02-12 13:50:27 -0500

    Make Window

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+from tkinter import *
+from tkinter import messagebox
+
+# Create interface
+clockWindow = Tk()
+clockWindow.geometry('200x200')
+clockWindow.title('Pomodoro Timer')
+clockWindow.configure(background='#1e1e1e')
+
+
+clockWindow.mainloop()
